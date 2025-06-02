43d402788df0888491c8d1413b1fc5d1277d72d8
Satilayzer <shafiev.dan@gmail.com>
2025-06-02 22:26:07 +0300

    --

--- ThreadsLab4/src/ChangingOrder/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Table table = new Table();
-
-        for(int i = 0; i < 5; i++) {
-            new Philosopher(i, table);
-        }
-    }
-}
